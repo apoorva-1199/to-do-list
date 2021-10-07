b1ae8adbcd1edf4692eb47c41dbdd1fbda263d1e
apoorva-1199 <apoorva.sharma@comprotechnologies.com>
2021-10-07 19:10:45 +0530

    commit changes

--- src/Data/test.js
+++
@@ -1,6 +0,0 @@
-export var todoItems = [];
-// todoItems.push({index: 1, value: "learn react", done: false});
-// todoItems.push({index: 2, value: "Go shopping", done: true});
-// todoItems.push({index: 3, value: "buy flowers", done: true});
-
-export var completedItems = [];
